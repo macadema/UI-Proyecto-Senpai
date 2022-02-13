dc5a2bb16642f11a72b347850f845d2c68373d32
Mariano Camacho <macadema@gmail.com>
2022-02-13 15:53:56 -0300

    Footer y Header definidos y funcionando

---
+++ proyecto-senpai/src/components/common/Header.jsx
@@ -0,0 +1,29 @@
+export function Header(){
+
+    return (
+    <>
+        <header>
+
+            <nav>
+                <a href="homepage.html">Home</a>
+                <a href="quienessomos.html">Quienes somos</a>
+                <a href="#">Proyectos</a>
+                <a href="contactanos.html">Contactanos</a>
+            </nav>
+
+                <section class="titulos-header" url="./ProyectoSenpai.html">
+                <h1>Sophie Interiores</h1>
+                <h2>Diseñando Sueños</h2>
+
+            </section>
+
+            <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path style= {{fill: "#cfdee7", fillOpacity: "1"}}
+                    d="M0,96L48,117.3C96,139,192,181,288,208C384,235,480,245,576,234.7C672,224,768,192,864,170.7C960,149,1056,139,1152,149.3C1248,160,1344,192,1392,208L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+                </path>
+            </svg>
+
+        </header>
+    </>
+    );
+}
